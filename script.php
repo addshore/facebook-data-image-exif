c571f38f3aa13e99eecb8d065c4e40c6fd8d0410
Jordan Schulz <jordan@webkey.us>
2018-08-06 16:24:41 -0400

    refactor for JSON, update examples and Readme

--- script.php
+++ script.php
@@ -1,88 +1,114 @@
 <?php
 
 // Licence GPL 2+ https://www.gnu.org/licenses/gpl-2.0.en.html
+// @TODO: Replace default UTC
+date_default_timezone_set('UTC');
 
-// Directory of the extracted facebook dump photos
-// Example: 'C:/Users/username/Downloads/facebook-username/photos'
-$directory = getenv( 'photosdirectory' );
-// Location of the exiftool script
-$tool = getenv( 'exiftool' );
+/**
+ * Read exported Facebook JSON for photos, apply exif data to originals
+ */
+class FacebookEXIF
+{
 
-echo "Starting\n";
-echo "Using exiftool location: '" . $tool . "'\n";
-echo "Using input location: '" . $directory . "'\n";
+	function __construct()
+	{
+		// Directory of the extracted facebook dump photos
+		// Example: 'C:/Users/username/Downloads/facebook-username/photos'
+		// Or mac: /Users/myname/Downloads/photos_and_videos
+		$this->directory = getenv( 'photosdirectory' );
+		// Location of the exiftool script
+		$this->tool = getenv( 'exiftool' );
 
-$albums = glob( $directory . '/*', GLOB_ONLYDIR );
+		echo "Starting\n";
+		echo "Using exiftool location: '" . $this->tool . "'\n";
+		echo "Using input location: '" . $this->directory . "'\n";
 
-echo "Got " . count( $albums ) . " albums from the input directory.\n";
+		$this->getPhotos();
 
-foreach ( $albums as $albumLocation ) {
-	$albumName = str_replace( $directory . DIRECTORY_SEPARATOR, '', $albumLocation );
-	$htmlLocation = $directory . DIRECTORY_SEPARATOR .  $albumName . '.html';
-	echo "Running for album $albumName\n";
-	$dom = DOMDocument::loadHTMLFile( $htmlLocation );
-	$finder = new DomXPath( $dom );
-	$blockNodes = $finder->query( "//*[contains(concat(' ', @class, ' '), ' block ')]" );
-	foreach ( $blockNodes as $blockNode ) {
-		$parsedUrl = parse_url( $blockNode->firstChild->getAttribute( 'src' ) );
+		echo "Done!\n";
 
-		// Break up the parsed URL a bit
-		$srcPath = $parsedUrl['path'];
-		$srcPathParts = explode( '/', $srcPath );
-		$srcFileName = array_pop( $srcPathParts );
-		$srcFileNameParts = explode( '_', $srcFileName );
-
-		// Set the file ID and type
-		$imgId = $srcFileNameParts[1];
-		// I'm pretty sure this is always actually jpg...
-		$imgType = explode( '.', array_pop( $srcFileNameParts ) )[1];
-
-		// Come up with the local file location
-		$imgLocation = $albumLocation . DIRECTORY_SEPARATOR . $imgId . '.' . $imgType;
-
-		echo "Running for file $imgLocation\n";
-
-		$details = array();
-		$metaDiv = $blockNode->lastChild;
-		$details['textContent'] = $metaDiv->firstChild->textContent;
-		$metaTable = $metaDiv->childNodes->item( 1 );
-		foreach ( $metaTable->childNodes as $rowNode ) {
-			$details[$rowNode->firstChild->textContent] = $rowNode->lastChild->textContent;
-		}
-
-		$toChange = [];
-
-		// TODO eww hardcoded UTC....
-		$toChange[] = '"-EXIF:ModifyDate=' . date_format( new DateTime( 'now', new DateTimeZone( 'UTC' ) ), 'Y:m:d G:i:s' ) . '"';
-
-		if ( array_key_exists( 'Taken', $details ) ) {
-			$toChange[] = '"-EXIF:DateTimeOriginal=' .
-				date_format( new DateTime( "@" . $details['Taken'], new DateTimeZone( 'UTC' ) ), 'Y:m:d G:i:s' ) .
-				'"';
-		} else {
-			continue;
-		}
-		if ( array_key_exists( 'Camera Make', $details ) ) {
-			$toChange[] = '"-EXIF:Make=' . $details['Camera Make'] . '"';
-		}
-		if ( array_key_exists( 'Camera Model', $details ) ) {
-			$toChange[] = '"-EXIF:Model=' . $details['Camera Model'] . '"';
-		}
-		// Doing this will cause odd rotations.... (as facebook has already rotated the image)...
-		//      if ( array_key_exists( 'Orientation', $details ) ) {
-		//          $toChange[] = '"-EXIF:Orientation=' . $details['Orientation'] . '"';
-		//      }
-		if ( array_key_exists( 'Latitude', $details ) && array_key_exists( 'Longitude', $details ) ) {
-			$toChange[] = '"-EXIF:GPSLatitude=' . $details['Latitude'] . '"';
-			$toChange[] = '"-EXIF:GPSLongitude=' . $details['Longitude'] . '"';
-			// Tool will look at the sign used for NSEW!
-			$toChange[] = '"-EXIF:GPSLatitudeRef=' . $details['Latitude'] . '"';
-			$toChange[] = '"-EXIF:GPSLongitudeRef=' . $details['Longitude'] . '"';
-			$toChange[] = '"-EXIF:GPSAltitude=' . '0' . '"';
-		}
-
-		exec( $tool . ' ' . implode( ' ', $toChange ) . ' ' . $imgLocation );
 	}
+
+	private function getPhotos()
+	{
+		$jsonAlbums = glob( $this->directory . '/album/*.json' );
+		if(empty($jsonAlbums)) {
+			throw new Exception("Could not find any JSON files in album directory");
+		}
+		$count = count( $jsonAlbums );
+		echo "Got " . $count . " albums from the input directory.\n";
+		foreach ($jsonAlbums as $i => $album) {
+			$json = json_decode(file_get_contents($album), true);
+			if(!$json || empty($json['photos'])) {
+				continue;
+			}
+			echo "\n=== Album $i/$count {$json['name']} ===\n\n";
+			$this->processPhotos($json['photos']);
+		}
+	}
+
+	private function processPhotos($photos)
+	{
+		if(empty($photos)) {
+			throw new Exception("No photo information");
+		}
+		// This array holds the EXIF name in the key and FB meta in the value
+		$possibleExif = array(
+			'Make' => "camera_make",
+			'Model' => "camera_model",
+			'DateTimeOriginal' => "taken_timestamp",
+			'ModifyDate' => "modified_timestamp",
+			'Exposure' => "exposure",
+			'FocalLength' => "focal_length",
+			'FNumber' => "f_stop",
+			'ISO' => "iso_speed",
+			'GPSLatitude' => "latitude",
+			'GPSLongitude' => "longitude",
+			'GPSLatitudeRef' => "latitude",
+			'GPSLongitudeRef' => "longitude",
+			// '' => "orientation", this will cause odd rotations (as facebook has already rotated the image)
+		);
+
+		// Loop through all photos, grab data and modify file
+		foreach ($photos as $photoData) {
+			$metaData = $photoData['media_metadata']['photo_metadata'];
+			$toChange = array();
+			if($metaData['taken_timestamp']) {
+				// Keep timestamp as is
+			} else if ($metaData['modified_timestamp']) {
+				// It's missing, replace with modified
+				$metaData['taken_timestamp'] = $metaData['modified_timestamp'];
+			} else {
+				$metaData['taken_timestamp'] = $photoData['creation_timestamp'];
+			}
+			$metaData['taken_timestamp'] = date('Y:m:d G:i:s', $metaData['taken_timestamp']);
+			if($metaData['modified_timestamp']) {
+				$metaData['modified_timestamp'] = date('Y:m:d G:i:s', $metaData['modified_timestamp']);
+			} else {
+				$metaData['modified_timestamp'] = date('Y:m:d G:i:s');
+			}
+			if($metaData['f_stop']) {
+				$parts = explode('/', $metaData['f_stop']);
+				if(count($parts) > 1) {
+					$metaData['f_stop'] = $parts[0] / $parts[1];
+				}
+			}
+			foreach ($possibleExif as $exifValue => $metaValue) {
+				if(array_key_exists($metaValue, $metaData)) {
+					$toChange[] = '"-EXIF:'.$exifValue.'=' . $metaData[$metaValue] . '"';
+				}
+			}
+			// Default a 0 altitude if lat/lon given
+			if(array_key_exists('latitude', $metaData)) {
+				$toChange[] = '"-EXIF:GPSAltitude=' . '0' . '"';
+			}
+			$imgLocation = $this->directory . str_replace('photos_and_videos', '', $photoData['uri']);
+			echo "Running for file $imgLocation\n";
+			exec( $this->tool . ' ' . implode( ' ', $toChange ) . ' -overwrite_original ' . $imgLocation );
+		}
+
+	}
+
 }
 
-echo "Done!\n";
+new FacebookEXIF;
