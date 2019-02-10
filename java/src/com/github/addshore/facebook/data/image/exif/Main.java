e77f8c412eaf0534babc3d88fd30bf09662846eb
addshore <addshorewiki@gmail.com>
2019-02-10 09:57:24 +0000

    Actually use background thread

--- java/src/com/github/addshore/facebook/data/image/exif/Main.java
+++ java/src/com/github/addshore/facebook/data/image/exif/Main.java
@@ -1,9 +1,5 @@
 package com.github.addshore.facebook.data.image.exif;
 
-import com.thebuzzmedia.exiftool.ExifTool;
-import com.thebuzzmedia.exiftool.ExifToolBuilder;
-import com.thebuzzmedia.exiftool.Tag;
-import com.thebuzzmedia.exiftool.core.StandardTag;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -18,15 +14,9 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
-import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 
 public class Main extends Application {
@@ -125,149 +115,28 @@ public class Main extends Application {
                 }
 
                 try {
-                    showProcessingScreen(stage, dirFile, exiftoolFile);
+
+                    // Overwrite the old output with the new, and start the task
+                    TextFlow textArea = new TextFlow();
+                    textArea.getChildren().add( new Text( "Processing... " ) );
+
+                    stage.setScene(new Scene(textArea, 800, 800));
+                    stage.show();
+
+                    ProcessingTask task = new ProcessingTask( textArea, dirFile, exiftoolFile );
+                    Thread th = new Thread(task);
+                    th.setDaemon(false);
+                    System.out.println("Main: pre thread start");
+                    th.start();
+                    System.out.println("Main: post thread start");
+
                 } catch (Exception e) {
                     e.printStackTrace();
-                    return;
                 }
             }
         });
 
         return new Scene(dataEntryView, 400, 200);
-    }
-
-    private void showProcessingScreen( Stage stage, File dir, File exiftoolFile ) throws Exception {
-        TextFlow textArea = new TextFlow();
-        textArea.getChildren().add( new Text( "Processing... " ) );
-
-        stage.setScene(new Scene(textArea, 800, 800));
-        stage.show();
-
-        doProcessing( textArea, dir, exiftoolFile );
-    }
-
-    /**
-     * @param textArea for output
-     */
-    private void doProcessing( TextFlow textArea, File dir, File exiftoolFile ) throws Exception {
-
-        // Find all album json files
-        textArea.getChildren().add( new Text( "Looking for albums... " ) );
-        File albumDir = new File( dir.toPath().toString() + "\\album" );
-
-
-        File[] albumJsonFiles = albumDir.listFiles(new FilenameFilter() {
-            public boolean accept(File dir, String filename)
-            { return filename.endsWith(".json"); }
-        } );
-
-        textArea.getChildren().add( new Text( albumJsonFiles.length + " albums found!\n" ) );
-
-        ExifToolBuilder builder = new ExifToolBuilder();
-        builder.withPath( exiftoolFile );
-
-        ExifTool exifTool = builder.build();
-
-        // Process the album
-        for (File albumJsonFile : albumJsonFiles) {
-            InputStream inputStream = new FileInputStream(albumJsonFile);
-
-            StringWriter writer = new StringWriter();
-            IOUtils.copy(inputStream, writer, "UTF-8");
-            String jsonTxt = writer.toString();
-            JSONObject albumJson = new JSONObject(jsonTxt);
-            if (!albumJson.has("photos")) {
-                continue;
-            }
-
-            textArea.getChildren().add(new Text("=== Album " + albumJson.getString("name") + " ===\n"));
-
-            // Process the photos in the album
-            JSONArray albumPhotos = albumJson.getJSONArray("photos");
-            for (int i = 0; i < albumPhotos.length(); i++) {
-                JSONObject photoData = albumPhotos.getJSONObject(i);
-                JSONObject photoMetaData = photoData.getJSONObject("media_metadata").getJSONObject("photo_metadata");
-
-                // Figure out the time the picture was taken
-                String takenTimestamp;
-                if (photoMetaData.has("taken_timestamp")) {
-                    // Keep timestamp as is
-                    takenTimestamp = photoMetaData.getString("taken_timestamp");
-                } else if (photoMetaData.has("modified_timestamp")) {
-                    // It's missing, replace with modified
-                    takenTimestamp = photoMetaData.getString("modified_timestamp");
-                } else {
-                    // Fallback to the creation timestamp
-                    takenTimestamp = photoMetaData.getString("creation_timestamp");
-                }
-                takenTimestamp = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss").format(new Date(Long.parseLong(takenTimestamp) * 1000));
-
-                // And set a modified timestamp
-                String modifiedTimestamp;
-                if (photoMetaData.has("modified_timestamp")) {
-                    modifiedTimestamp = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss").format(new Date(Long.parseLong(photoMetaData.getString("modified_timestamp")) * 1000));
-                } else {
-                    modifiedTimestamp = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss").format(new Date());
-                }
-
-                String fStop = null;
-                if (photoMetaData.has("f_stop")) {
-                    String[] parts = photoMetaData.getString("f_stop").split("/");
-                    if (parts.length > 1) {
-                        fStop = Double.toString(Double.parseDouble(parts[0]) / Double.parseDouble(parts[1]));
-                    } else {
-                        fStop = photoMetaData.getString("f_stop");
-                    }
-                }
-
-                File imageFile = new File(dir.toPath().toString() + "/../" + photoData.getString("uri"));
-
-                textArea.getChildren().add(new Text(" * Processing: " + photoData.getString("uri") + "\n"));
-
-                Map<Tag, String> exifData = new HashMap<Tag, String>();
-
-                exifData.put( CustomTag.MODIFYDATE, modifiedTimestamp );
-                exifData.put( StandardTag.DATE_TIME_ORIGINAL, takenTimestamp );
-
-                if( photoMetaData.has("camera_make") ) {
-                    exifData.put( StandardTag.MAKE, photoMetaData.getString("camera_make") );
-                }
-                if( photoMetaData.has("camera_model") ) {
-                    exifData.put( StandardTag.MODEL, photoMetaData.getString("camera_model") );
-                }
-
-                if( photoMetaData.has("latitude") ) {
-                    exifData.put( StandardTag.GPS_LATITUDE, photoMetaData.getString("latitude") );
-                    exifData.put( StandardTag.GPS_LATITUDE_REF, photoMetaData.getString("latitude") );
-                    exifData.put( StandardTag.GPS_LONGITUDE, photoMetaData.getString("longitude") );
-                    exifData.put( StandardTag.GPS_LONGITUDE_REF, photoMetaData.getString("longitude") );
-                    exifData.put( StandardTag.GPS_ALTITUDE, "0" );
-                    exifData.put( StandardTag.GPS_ALTITUDE_REF, "0" );
-                }
-
-                if( photoMetaData.has("exposure") ) {
-                    exifData.put( CustomTag.EXPOSURE, photoMetaData.getString("exposure") );
-                }
-                if( photoMetaData.has("iso_speed") ) {
-                    exifData.put( StandardTag.ISO, photoMetaData.getString("iso_speed") );
-                }
-                if( photoMetaData.has("focal_length") ) {
-                    exifData.put( StandardTag.FOCAL_LENGTH, photoMetaData.getString("focal_length") );
-                }
-                if(fStop != null) {
-                    exifData.put( CustomTag.FNUMBER, fStop );
-                }
-
-                exifData.put( StandardTag.COMMENT, "EXIF data converted from facebook dump using https://github.com/addshore/facebook-data-image-exif" );
-
-                exifTool.setImageMeta( imageFile, exifData );
-
-            }
-        }
-
-        textArea.getChildren().add(new Text(" Done!!\n"));
-        exifTool.close();
-
     }
 
 }
---
+++ java/src/com/github/addshore/facebook/data/image/exif/ProcessingTask.java
@@ -0,0 +1,176 @@
+package com.github.addshore.facebook.data.image.exif;
+
+import com.thebuzzmedia.exiftool.ExifTool;
+import com.thebuzzmedia.exiftool.ExifToolBuilder;
+import com.thebuzzmedia.exiftool.Tag;
+import com.thebuzzmedia.exiftool.core.StandardTag;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProcessingTask extends Task {
+
+    private TextFlow textArea;
+    private File dir;
+    private File exiftoolFile;
+    private String stateMessage;
+
+    ProcessingTask(TextFlow textArea, File dir, File exiftoolFile){
+        this.textArea = textArea;
+        this.dir = dir;
+        this.exiftoolFile = exiftoolFile;
+        this.stateMessage = "Task started!";
+    }
+
+    private void appendMessage( String string ) {
+        System.out.println("ProcessingTask: " + string);
+        stateMessage = stateMessage + "\n" + string;
+        updateUI();
+    }
+
+    private void updateUI() {
+        Runnable updater = new Runnable() {
+            @Override
+            public void run() {
+                updateMessage( stateMessage );
+                textArea.getChildren().set(0, new Text(stateMessage));
+            }
+        };
+
+        // UI update is run on the Application thread
+        Platform.runLater(updater);
+    }
+
+    @Override
+    protected Object call() throws Exception {
+        // Find all album json files
+        appendMessage( "Looking for albums... " );
+        File albumDir = new File( dir.toPath().toString() + "\\album" );
+
+        File[] albumJsonFiles = albumDir.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String filename)
+            { return filename.endsWith(".json"); }
+        } );
+
+        appendMessage(albumJsonFiles.length + " albums found!");
+
+        ExifToolBuilder builder = new ExifToolBuilder();
+        builder.withPath( exiftoolFile );
+
+        ExifTool exifTool = builder.build();
+
+        // Process the album
+        for (File albumJsonFile : albumJsonFiles) {
+            InputStream inputStream = new FileInputStream(albumJsonFile);
+
+            StringWriter writer = new StringWriter();
+            IOUtils.copy(inputStream, writer, "UTF-8");
+            String jsonTxt = writer.toString();
+            JSONObject albumJson = new JSONObject(jsonTxt);
+            if (!albumJson.has("photos")) {
+                continue;
+            }
+
+            appendMessage("=== Album " + albumJson.getString("name") + " ===");
+
+            // Process the photos in the album
+            JSONArray albumPhotos = albumJson.getJSONArray("photos");
+            for (int i = 0; i < albumPhotos.length(); i++) {
+                JSONObject photoData = albumPhotos.getJSONObject(i);
+                JSONObject photoMetaData = photoData.getJSONObject("media_metadata").getJSONObject("photo_metadata");
+
+                // Figure out the time the picture was taken
+                String takenTimestamp;
+                if (photoMetaData.has("taken_timestamp")) {
+                    // Keep timestamp as is
+                    takenTimestamp = photoMetaData.getString("taken_timestamp");
+                } else if (photoMetaData.has("modified_timestamp")) {
+                    // It's missing, replace with modified
+                    takenTimestamp = photoMetaData.getString("modified_timestamp");
+                } else {
+                    // Fallback to the creation timestamp
+                    takenTimestamp = photoMetaData.getString("creation_timestamp");
+                }
+                takenTimestamp = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss").format(new Date(Long.parseLong(takenTimestamp) * 1000));
+
+                // And set a modified timestamp
+                String modifiedTimestamp;
+                if (photoMetaData.has("modified_timestamp")) {
+                    modifiedTimestamp = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss").format(new Date(Long.parseLong(photoMetaData.getString("modified_timestamp")) * 1000));
+                } else {
+                    modifiedTimestamp = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss").format(new Date());
+                }
+
+                String fStop = null;
+                if (photoMetaData.has("f_stop")) {
+                    String[] parts = photoMetaData.getString("f_stop").split("/");
+                    if (parts.length > 1) {
+                        fStop = Double.toString(Double.parseDouble(parts[0]) / Double.parseDouble(parts[1]));
+                    } else {
+                        fStop = photoMetaData.getString("f_stop");
+                    }
+                }
+
+                File imageFile = new File(dir.toPath().toString() + "/../" + photoData.getString("uri"));
+
+                appendMessage(" * Processing: " + photoData.getString("uri"));
+
+                Map<Tag, String> exifData = new HashMap<Tag, String>();
+
+                exifData.put( CustomTag.MODIFYDATE, modifiedTimestamp );
+                exifData.put( StandardTag.DATE_TIME_ORIGINAL, takenTimestamp );
+
+                if( photoMetaData.has("camera_make") ) {
+                    exifData.put( StandardTag.MAKE, photoMetaData.getString("camera_make") );
+                }
+                if( photoMetaData.has("camera_model") ) {
+                    exifData.put( StandardTag.MODEL, photoMetaData.getString("camera_model") );
+                }
+
+                if( photoMetaData.has("latitude") ) {
+                    exifData.put( StandardTag.GPS_LATITUDE, photoMetaData.getString("latitude") );
+                    exifData.put( StandardTag.GPS_LATITUDE_REF, photoMetaData.getString("latitude") );
+                    exifData.put( StandardTag.GPS_LONGITUDE, photoMetaData.getString("longitude") );
+                    exifData.put( StandardTag.GPS_LONGITUDE_REF, photoMetaData.getString("longitude") );
+                    exifData.put( StandardTag.GPS_ALTITUDE, "0" );
+                    exifData.put( StandardTag.GPS_ALTITUDE_REF, "0" );
+                }
+
+                if( photoMetaData.has("exposure") ) {
+                    exifData.put( CustomTag.EXPOSURE, photoMetaData.getString("exposure") );
+                }
+                if( photoMetaData.has("iso_speed") ) {
+                    exifData.put( StandardTag.ISO, photoMetaData.getString("iso_speed") );
+                }
+                if( photoMetaData.has("focal_length") ) {
+                    exifData.put( StandardTag.FOCAL_LENGTH, photoMetaData.getString("focal_length") );
+                }
+                if(fStop != null) {
+                    exifData.put( CustomTag.FNUMBER, fStop );
+                }
+
+                exifData.put( StandardTag.COMMENT, "EXIF data converted from facebook dump using https://github.com/addshore/facebook-data-image-exif" );
+
+                exifTool.setImageMeta( imageFile, exifData );
+
+            }
+        }
+
+        appendMessage(" Done!!");
+
+        exifTool.close();
+
+        return null;
+    }
+
+}
