bab8d675ea98e522e43148f7b0cb69862d01e6a5
addshore <addshorewiki@gmail.com>
2019-03-01 13:17:02 +0000

    Bump version to 0.8

--- java/src/com/github/addshore/facebook/data/image/exif/Main.java
+++ java/src/com/github/addshore/facebook/data/image/exif/Main.java
@@ -21,7 +21,7 @@ import java.util.Objects;
 
 public class Main extends Application {
 
-    private String version = "0.7";
+    private String version = "0.8";
     private TextField toolInput;
     private TextField dirInput;
     private CheckBox debugCheckbox;
