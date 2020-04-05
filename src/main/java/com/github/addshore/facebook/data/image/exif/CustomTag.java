d7b36f1b49fac44169466626798b6d1d6b1a1d64
addshore <adamshorland@gmail.com>
2020-04-05 14:08:10 +0100

    General cleanup, pom indenting and build name

--- src/main/java/com/github/addshore/facebook/data/image/exif/CustomTag.java
+++ src/main/java/com/github/addshore/facebook/data/image/exif/CustomTag.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
 package com.github.addshore.facebook.data.image.exif;
 
 import com.thebuzzmedia.exiftool.Constants;
